b826cff80bc8e10fe69b09155cde189e4ea52209
JagoanNe0n <141192406+JagoanNe0n@users.noreply.github.com>
2023-08-23 21:41:22 +0700

    stagesLaliga.py

--- stagesLaliga/stagesLaliga.py
+++ stagesLaliga/stagesLaliga.py
@@ -31,28 +31,3 @@ for stage in stages:
         total = team['ptsn']
         
         print(f"klasemen {klasemen} === peringkat {ranking} {club}, Menang : {menang}, Kalah : {kalah}, Total Nilai : {total}")
-
-
-# data_list = [5,6,'Jogja','Nurma',555, 666]
-# ranking = jsondata['Stages'][0]['LeagueTable']['L'][0]['Tables'][0]['team'][0]['rnk']
-
-# for d, i in enumerate(data_list, start=1):
-#     print(f"value dari dta index {d} adalah {i}")
-
-
-#     rank = club['rnk']
-#     team = club['Tnm']
-#     menang  = club['win']
-#     kalah = club['wot']
-#     total = club['ptsn']
-#     print(f"klasemen {klasemen} === peringkat {ranking} {club}, Menang : {menang}, Kalah : {kalah}, Total Nilai : {total}")
-
-# klasemen = jsondata['Stages'][0]['Snm']
-# ranking = jsondata['Stages'][0]['LeagueTable']['L'][0]['Tables'][0]['team'][0]['rnk']
-# club = jsondata['Stages'][0]['LeagueTable']['L'][0]['Tables'][0]['team'][0]['Tnm']
-# menang = jsondata['Stages'][0]['LeagueTable']['L'][0]['Tables'][0]['team'][0]['win']
-# kalah = jsondata['Stages'][0]['LeagueTable']['L'][0]['Tables'][0]['team'][0]['wot']
-# total = jsondata['Stages'][0]['LeagueTable']['L'][0]['Tables'][0]['team'][0]['ptsn']
-# print(f"klasemen {klasemen} === peringkat {ranking} {club}, Menang : {menang}, Kalah : {kalah}, Total Nilai : {total}")
-# banyak = len(klasemen)
-# print(banyak)
